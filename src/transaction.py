23cd1296dfc6972a18f2ec025d5d9ebe42fe459b
mdan-123 <118456325+mdan-123@users.noreply.github.com>
2025-03-12 21:57:08 +0000

    Add files via upload
    transaction class

---
+++ src/transaction.py
@@ -0,0 +1,10 @@
+class Transaction:
+    def __init__(self, timestamp, amount, hash, sender, receiver):
+        self.timestamp = timestamp
+        self.amount = amount
+        self.hash = hash
+        self.sender = sender
+        self.receiver = receiver
+        
+        
+        
