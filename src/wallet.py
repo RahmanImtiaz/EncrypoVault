cad62851fd7ddc3165aa4877683a72ef75e5ef2f
mdan-123 <118456325+mdan-123@users.noreply.github.com>
2025-03-12 21:56:46 +0000

    Add files via upload
    Wallet class, also has an attribute for cryptowatch

---
+++ src/wallet.py
@@ -0,0 +1,18 @@
+from cryptoWatch import CryptoWatch
+
+class Wallet:
+    def __init__(self, name: str):
+        self.balance = 0.0
+        self.address = None
+        self.name = name
+        self.watch = None
+
+
+    def link_to_watch(self, watch):
+        if self.watch is not None and self in self.watch.wallets:
+            self.watch.wallets.remove(self)
+        self.watch = watch
+        if watch is not None and self not in watch.wallets:
+            watch.wallets.append(self)
+            
+            
