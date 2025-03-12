3277faf5abde4e3b72aa682434456c0ed7bd87f1
mdan-123 <118456325+mdan-123@users.noreply.github.com>
2025-03-12 21:55:55 +0000

    Add files via upload
    This is the crypto class, with an additional method not specified that adds the last modified attribute

---
+++ src/crypto.py
@@ -0,0 +1,14 @@
+import datetime
+
+class Crypto:
+    def __init__(self, name: str):
+        self.name = name
+        self.conversion_rate = 0.0
+        self.last_updated = None
+        
+        
+    def update_price(self, new_price: float):
+        self.conversion_rate = new_price
+        self.last_updated = datetime.datetime.now()
+    
+        
