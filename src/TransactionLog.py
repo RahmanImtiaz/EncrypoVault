4d2b75b12c53995826613e8a142cef0cf8e10fc8
flaxeneel2 <flaxeneel2@gmail.com>
2025-03-13 12:21:05 +0000

    fixed `hash` shadowing the python hash() function

--- src/TransactionLog.py
+++ src/TransactionLog.py
@@ -5,9 +5,9 @@ class TransactionLog:
     def __init__(self):
         self._log = []
         
-    def search(self, timestamp: datetime, amount: float, hash: str, sentTo: str, sentBy: str):
+    def search(self, timestamp: datetime, amount: float, tx_hash: str, sent_to: str, sent_by: str):
         for transaction in self._log:
-            if transaction.timestamp == timestamp and transaction.amount == amount and transaction.hash == hash and transaction.sentTo == sentTo and transaction.sentBy == sentBy:
+            if transaction.timestamp == timestamp and transaction.amount == amount and transaction.hash == tx_hash and transaction.sentTo == sent_to and transaction.sentBy == sent_by:
                 return transaction
         return None
         
