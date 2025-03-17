f00f411c6417a06bcb0bec5e98c8ca67a4e69d31
mdan-123 <118456325+mdan-123@users.noreply.github.com>
2025-03-17 22:56:20 +0000

    Fixed the update method in wallet. it now updates the balance correctly.

--- src/Wallet.py
+++ src/Wallet.py
@@ -10,12 +10,12 @@ class Wallet(CryptoObserver):
         
     def update(self, crypto_name: str, new_price: float):
         if crypto_name in self.holdings:
-            quantity = self.holdings[crypto_name]
-            value = quantity * new_price
-            self.holdings[crypto_name] = value
-            print("Updated " + crypto_name + " value to " + str(value))
+            holding = self.holdings[crypto_name]
+            holding["value"] = holding["quantity"] * new_price
+            print("Updated " + crypto_name + " value to " + str(holding["value"]))
         else:
             print("Could not update " + crypto_name + " value")
+
 
 
 
