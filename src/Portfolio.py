3643a804f34891429d36c5267e6eb6047fd36c8d
mdan-123 <118456325+mdan-123@users.noreply.github.com>
2025-03-18 13:06:01 +0000

    Created the Portfolio class

---
+++ src/Portfolio.py
@@ -0,0 +1,28 @@
+class Portfolio:
+    def __init__(self, name: str):
+        self.name = name
+        self.wallets = []
+        self.total_balance = 0.0
+        
+        
+        
+    #unsure if these method are needed
+    def add_wallet(self, wallet):
+        self.wallets.append(wallet)
+        
+    
+    #unsure if these method are needed
+    def remove_wallet(self, wallet):
+        self.wallets.remove(wallet)
+        
+        
+    def get_total_balance(self):
+        return self.total_balance
+    
+    def update_total_balance(self):
+        for i in range(len(self.wallets)):
+            self.total_balance += self.wallets[i].balance
+
+        
+    
+    
